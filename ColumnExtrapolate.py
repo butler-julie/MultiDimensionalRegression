598430ad95f326e2f36934c3808becbc58fc959b
Julie Butler Hartley <juliehartley1121@gmail.com>
2021-01-31 14:59:34 -0500

    Create ColumnExtrapolate.py

---
+++ ColumnExtrapolate.py
@@ -0,0 +1,16 @@
+##################################################
+# Column Extrapolation
+# Julie Butler Hartley
+# Version 0.0.0
+# Date Created: January 31, 2021
+# Last Modified: January 31, 2021
+#
+# A program created to increase the number of columns in a matrix
+# via sequential data formatting and regression algorithms.
+##################################################
+
+##############################
+#IMPORTS
+##############################
+import numpy as np
+import matplotlib.pyplot as plt
