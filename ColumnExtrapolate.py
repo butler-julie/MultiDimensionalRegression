a4d955fdb420e6cc941764a7392e298a001a9427
Julie Butler Hartley <juliehartley1121@gmail.com>
2021-02-01 12:19:26 -0500

    Update ColumnExtrapolate.py

--- ColumnExtrapolate.py
+++ ColumnExtrapolate.py
@@ -14,3 +14,29 @@
 ##############################
 import numpy as np
 import matplotlib.pyplot as plt
+
+##############################
+#FormatIMSRGData
+##############################
+def formatIMSRGdata (filename):
+  "do nothing now"
+##############################
+#FormatFunctionData
+##############################
+def formatFunctionData (func, X, Y):
+  return func(X, Y)
+##############################
+#LinearRegression
+##############################
+def LinearRegression(data):
+  rows, cols = data.shape()
+  design_matrix = np.zeros((cols-2, 2, rows))
+  y_data = np.zeros((cols-2, m))
+  
+  for i in range (0, n-1):
+    design_matrix[i][0] = data[:,i]
+    desig_matrix[i][1] = data[:,i+1]
+  
+##############################
+#RidgeRegression
+##############################
